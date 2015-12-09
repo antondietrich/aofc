9621c7ddda378509fb0bc95eb6d5c6c1b44ce155
Anton Korenchikov <a.korenchikov@yandex.ru>
2015-12-09 09:06:35 +0300

    Generic puzzle solver

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+
+$runSolver = false;
+
+if( isset( $_GET['day'] ) && isset( $_GET['puzzle'] ) )
+{
+	$day = $_GET[ 'day' ];
+	$puzzle = $_GET[ 'puzzle' ];
+
+	$dir = "/day$day/";
+	$jsFile = "puzzle$puzzle.js";
+	$solverPath = $dir . $jsFile;
+	$inputPath = dirname( __FILE__ ) . $dir . 'input';
+
+	$runSolver = true;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+	$title = '';
+	if( $runSolver ) {
+		$title = "AOfC day $day";
+	}
+	else {
+		$title = "AOfC home";
+	}
+	echo "<title>$title</title>";
+	?>
+
+	<style type="text/css">
+	body {
+		font: 14px 'Fira Mono';
+	}
+	h1 {
+		font-size: 16px;
+	}
+	</style>
+	<?php
+	if( $runSolver ) {
+		echo "<script type='text/javascript' src='$solverPath'></script>";
+		echo "<script type='text/javascript' src='solver.js'></script>";
+	}
+	?>
+</head>
+<body>
+<?php
+if( !$runSolver )
+{
+	for( $day = 1; $day <= 25; $day++ )
+	{
+		$link1 = 'Puzzle 1';
+		$link2 = 'Puzzle 2';
+		if( file_exists( "day$day/puzzle1.js" ) )
+		{
+			$link1 = "<a href='?day=$day&puzzle=1'>Puzzle 1</a>";
+		}
+
+		if( file_exists( "day$day/puzzle2.js" ) )
+		{
+			$link2 = "<a href='?day=$day&puzzle=2'>Puzzle 2</a>";
+		}
+
+		echo '<pre>';
+		print( "<p>Day $day:\t$link1 &middot; $link2</p>" );
+		echo '</pre>';
+	}
+}
+else
+{
+	echo "<h1>Day $day Puzzle $puzzle</h1>";
+	echo '<pre id="input" hidden>';
+	echo file_get_contents( $inputPath );
+	echo "</pre>";
+	echo 'Result: <pre id="output" style="display: inline;"></pre>';
+	echo '<p><a href="/">Home</a></p>';
+}
+?>
+</body>
+</html>
---
+++ solver.js
@@ -0,0 +1,29 @@
+window.onload = Init;
+
+function Init()
+{
+	if( !SolverProcedure )
+	{
+		return;
+	}
+
+	var input = document.getElementById( "input" );
+	// ProcessFile( e, SolverProcedure );
+	Print( SolverProcedure( input.innerHTML ) );
+}
+
+function Print( text )
+{
+	var output = document.getElementById( "output" );
+	output.innerHTML = text;
+}
+
+function ProcessFile( e, callback )
+{
+	var blob = e.target.files[0];
+	var reader = new FileReader();
+	reader.onload = function( e ) {
+		Print( callback( e.target.result ) );
+	}
+	reader.readAsText( blob );
+}
