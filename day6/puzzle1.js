a3ebf6a6640fb1bb7e231fec7f07e72fba53bd6a
Anton Korenchikov <a.korenchikov@yandex.ru>
2015-12-10 17:53:45 +0300

    Day 6

---
+++ day6/puzzle1.js
@@ -0,0 +1,102 @@
+var STRIDE = 1000;
+var COMMAND_TURN_ON = 0;
+var COMMAND_TURN_OFF = 1;
+var COMMAND_TOGGLE = 2;
+
+function SolverProcedure( input )
+{
+    var output = 0;
+    var numTurnedOn = 0;
+
+    var lights = new Array( STRIDE*STRIDE );
+    var numLights = lights.length;
+    for( var i = 0; i < numLights; i++ )
+    {
+        lights[i] = 0;
+    }
+
+    var lines = input.split( "\n" );
+    for( var i = 0; i < lines.length; i++ )
+    {
+        var command = GetCommand( lines[i] );
+        var range = GetRange( lines[i] );
+        switch( command )
+        {
+            case COMMAND_TURN_ON:
+                SetLights( lights, range, 1 );
+                break;
+            case COMMAND_TURN_OFF:
+                SetLights( lights, range, 0 );
+                break;
+            case COMMAND_TOGGLE:
+                ToggleLights( lights, range );
+                break;
+        }
+    }
+
+    for( var y = 0; y < STRIDE; y++ )
+    {
+    	for( var x = 0; x < STRIDE; x++ )
+    	{
+    		if( lights[ x + y*STRIDE ] )
+    		{
+    			++numTurnedOn;
+    		}
+    	}
+    }
+
+    output = numTurnedOn;
+    return output;
+}
+
+function SetLights( lights, range, state )
+{
+    for( var y = range.min.y; y <= range.max.y; y++ )
+    {
+	    for( var x = range.min.x; x <= range.max.x; x++ )
+	    {
+            lights[x + y*STRIDE] = state;
+	    }
+    }
+}
+
+function ToggleLights( lights, range )
+{
+	for( var y = range.min.y; y <= range.max.y; y++ )
+    {
+	    for( var x = range.min.x; x <= range.max.x; x++ )
+	    {
+            lights[x + y*STRIDE] = !lights[x + y*STRIDE];
+	    }
+    }
+}
+
+function GetCommand( line )
+{
+    if( line.indexOf( "toggle" ) == 0 )
+            return COMMAND_TOGGLE;
+    if( line.indexOf( "turn on" ) == 0 )
+        return COMMAND_TURN_ON;
+    if( line.indexOf( "turn off" ) == 0 )
+        return COMMAND_TURN_OFF;
+}
+
+function GetRange( line )
+{
+	var i = 0;
+	for( ;; )
+	{
+		if( !isNaN( line[ i ] ) && line[ i ] !== " " )
+			break;
+		++i;
+	}
+
+	var rangeString = line.substr( i );
+	rangeString = rangeString.split( "through" );
+	var rangeMin = rangeString[0].split( ',' );
+	var rangeMax = rangeString[1].split( ',' );
+	var range = { min: { x: parseInt( rangeMin[0] ), y: parseInt( rangeMin[1] ) },
+				  max: { x: parseInt( rangeMax[0] ), y: parseInt( rangeMax[1] ) } };
+
+	return range;
+}
