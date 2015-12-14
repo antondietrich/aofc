43939f936bf8f8623923ca9a76b57d814ef4a0d7
Anton Korenchikov <a.korenchikov@yandex.ru>
2015-12-14 12:54:57 +0300

    Day 10

---
+++ day10/puzzle2.js
@@ -0,0 +1,41 @@
+function SolverProcedure( input )
+{
+	var output = 0;
+
+	input = input.trim();
+	var numIterations = 50;
+	for( var i = 0; i < numIterations; i++ )
+	{
+		input = LookAndSay( input );
+	}
+
+	output = input.length;
+	return output;
+}
+
+function LookAndSay( s )
+{
+	var result = "";
+
+	var currentChar = "";
+	var lastChar = "";
+	var currentCount = 0;
+	for( var i = 0; i < s.length; i++ )
+	{
+		currentChar = s[i];
+		if( currentChar != lastChar )
+		{
+			if( currentCount )
+			{
+				result += "" + currentCount + "" + lastChar;
+				currentCount = 0;
+			}
+		}
+		lastChar = currentChar;
+		++currentCount;
+	}
+
+	result += "" + currentCount + "" + lastChar;
+
+	return result;
+}
