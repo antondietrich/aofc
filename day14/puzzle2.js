7b4ae09c0ea005b154cebc870e79f0964eb90280
Anton Korenchikov <korenchikov19@gmail.com>
2015-12-16 17:37:53 +0300

    Day 14

---
+++ day14/puzzle2.js
@@ -0,0 +1,121 @@
+function SolverProcedure( input )
+{
+	var lines = input.split( "\n" );
+	var deer = [];
+
+	for( var i = 0; i < lines.length; i++ )
+	{
+		// remove trailing dot
+		lines[i] = lines[i].substr( 0, lines[i].length - 1 );
+
+		var parts = lines[i].split( " can fly " );
+		var deerName = parts[0];
+
+
+		var token = "";
+		var t = 0;
+		while( isNumeric( parts[1][t] ) )
+		{
+			token += parts[1][t];
+			t++;
+		}
+
+		var deerSpeed = parseInt( token );
+
+		token = "";
+		t = parts[1].indexOf( "for " ) + 4;
+		while( isNumeric( parts[1][t] ) )
+		{
+			token += parts[1][t];
+			t++;
+		}
+		var deerActivePerioid = parseInt( token );
+
+		token = "";
+		t = parts[1].lastIndexOf( "for " ) + 4;
+		while( isNumeric( parts[1][t] ) )
+		{
+			token += parts[1][t];
+			t++;
+		}
+		var deerRestPerioid = parseInt( token );
+
+
+		deer.push( { name: deerName, speed: deerSpeed, active: deerActivePerioid, rest: deerRestPerioid } );
+	}
+
+	var deerTracker = {};
+	for( var i = 0; i < deer.length; i++ )
+	{
+		deerTracker[ deer[i].name ] = { activeCounter: 0, restCounter: 0, isResting: false, distance: 0, score: 0 };
+	}
+
+	var timeToGo = 2503;
+	// var timeToGo = 1000;
+
+	for( var second = 0; second < timeToGo; second++ )
+	{
+		for( var i = 0; i < deer.length; i++ )
+		{
+			var d = deer[i];
+			if( !deerTracker[ d.name ].isResting )
+			{
+				deerTracker[ d.name ].distance += d.speed;
+				deerTracker[ d.name ].activeCounter++;
+				if( deerTracker[ d.name ].activeCounter == d.active )
+				{
+					deerTracker[ d.name ].isResting = true;
+				}
+			}
+			else
+			{
+				deerTracker[ d.name ].restCounter++;
+				if( deerTracker[ d.name ].restCounter == d.rest )
+				{
+					deerTracker[ d.name ].restCounter = 0;
+					deerTracker[ d.name ].activeCounter = 0;
+					deerTracker[ d.name ].isResting = false;
+				}
+			}
+		}
+
+		var winningDistance = 0;
+		var winningDeer = 0;
+		for( var k = 0; k < deer.length; k++ )
+		{
+			if( deerTracker[ deer[k].name ].distance > winningDistance )
+			{
+				winningDistance = deerTracker[ deer[k].name ].distance;
+				winningDeer = k;
+			}
+		}
+
+		for( var i = 0; i < deer.length; i++ )
+		{
+			if( deerTracker[ deer[i].name ].distance == winningDistance )
+				deerTracker[ deer[i].name ].score++;
+		}
+
+	}
+
+	var winningScore = 0;
+	for( var i = 0; i < deer.length; i++ )
+	{
+		if( deerTracker[ deer[i].name ].score > winningScore )
+			winningScore = deerTracker[ deer[i].name ].score;
+	}
+
+
+	var output = winningScore;
+	return output;
+}
+
+var numericChars = [
+	"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+];
+function isNumeric( c )
+{
+	if( numericChars.indexOf( c ) >= 0 )
+		return true;
+	return false;
+}
